88ace964e7cd1c2777f487dfa87aa1d71a0f680d
Rovdyr_UlV <rovdyrulv@gmail.com>
2024-02-07 15:27:04 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,13 @@
-public class Main {
- static void printThreeWords(){
-        System.out.println(
-                "Orange\nBanana\nApple"
-        );
- }
- static void checkSumSign(){
-        int a = 10;
-        int b = 20;
-        int c = a + b;
-        if (c > 0){
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
-        }
- }
- static void printColor(){
-     int value = 101;
-     if (value <= 0){
-         System.out.println("Красный");
-     } else if (value > 0 && value <= 100) {
-         System.out.println("Желтый");
-     } else if (value > 100){
-         System.out.println("Зеленый");
-     }
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-}
-static void compareNumbers(){
-     int a = 122;
-     int b = 101;
-     if( a >= b ){
-        System.out.println("a >= b");
-    } else {
-         System.out.println("a < b");
-    }
-}
+public class Main {
+    static String[] array = {"Отвертка", "Гайки", "Гвозди", "Болты", "Шуроповерт", "Ключ гаечный",
+            "Ключ разводной", "Шестигранник", "Молоток", "Изолента", "Гвозди", "Скотч", "Отвертка"};
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+        Set<String> arrayfilter = new HashSet<String>(Arrays.asList(array));
+        System.out.println(arrayfilter);
+        System.out.println(arrayfilter.size());
     }
 }
---
+++ src/Phonebook.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Phonebook {
+    private static Map<String, String> phonebook;
+    Phonebook() {
+        phonebook = new HashMap<>();
+    }
+    public void add(String number, String surname) {
+        phonebook.put(number, surname);
+    }
+    public void get(String surname){
+        if(phonebook.containsValue(surname)) {
+            Set<Map.Entry<String, String>> set = phonebook.entrySet();
+            for (Map.Entry<String, String> temp : set) {
+                if(temp.getValue().equals(surname)) {
+                    System.out.println(temp.getValue() + " : " + temp.getKey());
+                }
+            }
+        } else {
+            System.out.println("Такой фамилии нет в списке.");
+        }
+    }
+}
